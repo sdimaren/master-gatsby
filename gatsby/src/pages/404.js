4bf8f6f78f7636d06042f1159517669ad2408ba4
Santiago Dimaren <sdimaren1@gmail.com>
2021-03-05 15:57:06 -0500

    routing, navigation and layouts

--- gatsby/src/pages/404.js
+++ gatsby/src/pages/404.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function FourOhFour() {
   return (
-    <div>
+    <>
       <p>That page doesn't exist!</p>
-    </div>
+    </>
   );
 }
--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function BeersPage() {
   return (
-    <div>
+    <>
       <p>Beers Page</p>
-    </div>
+    </>
   );
 }
