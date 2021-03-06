45e44d204991ee2ab33c62abbcc25f25f32d7194
Santiago Dimaren <sdimaren1@gmail.com>
2021-03-06 14:37:02 -0500

    styling layout

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -17,6 +17,7 @@ const NavStyles = styled.nav`
     text-align: center;
     list-style: none;
     align-items: center;
+    margin-top: -6rem;
   }
   li {
     --rotate: -2deg;
@@ -41,6 +42,9 @@ const NavStyles = styled.nav`
     &:hover {
       color: var(--red);
     }
+    /* &[aria-current='page'] {
+      color: var(--red);
+    } */
   }
 `;
 
