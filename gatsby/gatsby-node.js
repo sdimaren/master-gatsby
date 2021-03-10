c956cda9446f270968f956cd8aff2460630e2a67
Santiago Dimaren <sdimaren1@gmail.com>
2021-03-10 17:05:16 -0500

    dynamically creating pages with gatsby-node

---
+++ gatsby/gatsby-node.js
@@ -0,0 +1,32 @@
+import { graphql } from 'gatsby';
+import path from 'path';
+
+async function turnPizzasIntoPages({ graphql, actions }) {
+  const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
+  const { data } = await graphql(`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  console.log(data);
+  data.pizzas.nodes.forEach((pizza) => {
+    actions.createPage({
+      path: `pizza/${pizza.slug.current}`,
+      component: pizzaTemplate,
+      context: {
+        slug: pizza.slug.current,
+      },
+    });
+  });
+}
+
+export async function createPages(params) {
+  await turnPizzasIntoPages(params);
+}
